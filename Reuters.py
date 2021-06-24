5f97fe578418a50661265371cf64c2237a8afe6e
Ciroso <arman.lagevardi@hotmail.com>
2021-06-25 00:12:45 +0200

    La magia del natale ha aggiustato tutto (quasi)

--- Reuters.py
+++ Reuters.py
@@ -1,151 +1,120 @@
-import fileinput
 from pathlib import Path
-from nltk.corpus import reuters
-
-from Preprocessing import body_extractor
-
-
-def forse():
-    from bs4 import BeautifulSoup, SoupStrainer
-    f = open('reuters21578/reut2-000.sgm', 'r')
-    data= f.read()
-    soup = BeautifulSoup(data,features="html.parser")
-    contents = soup.findAll('body')
-    for content in contents:
-        print(content.text)
-
-    contentss = soup.findAll('topics')
-    for content in contents:
-        print(content.text)
-
-    content = soup.findAll("reuters")
-    print()
-
-reuter_docs = []
-for i in range(0,22):
-    if i < 10:
-        reuter_docs.append("reuters21578/reut2-00" + str(i) + ".sgm")
+'''
+if use_cache:
+    print("Utilizzo la cache di top10categorie reuters")
+    if Path(r"cacheTop10.txt").exists():
+        with open("cacheTop10.txt", 'r') as f:
+            top10 = [line.rstrip('\n') for line in f]
+        return top10
     else:
-        reuter_docs.append("reuters21578/reut2-0" + str(i) + ".sgm")
-
-
-#def unga():
-
-def categorie_freq(use_cache=True):
-    if use_cache:
-        if Path(r"cacheTop10.txt").exists():
-            with open("cacheTop10.txt", 'r') as f:
-                top10 = [line.rstrip('\n') for line in f]
-            return top10
-
-    print('Creazione file categorie...\n')
-    filenames = ['reuters21578/all-topics-strings.lc.txt']
-
-    file = []
-    with fileinput.input(filenames) as fin:
-        for line in fin:
-            file.append(line)
-    print('Inserimento categorie in lista..\n')
-
-    categories = []
-    parts = []
-    for line in file:
-        parts = line.rstrip()
-        parts = line.split(' ')
-        parts = [p.rstrip() for p in parts]
-        categories.append(parts)
-    #print('Categorie:' + str(categories))
-    print('Numero delle categorie totali: ' + str(len(categories)))
-
-    print('Inizio conteggio occorrenze')
-    # CONTO QUANTE OCCORRENZE DI CIASCUNA CATEGORIA CI SONO#
-    nomi = {}
-    for i in range(len(categories)):
-        nomi[i] = categories[i]
-    #print('dizionario delle categorie:\n' + str(nomi))
-    conteggi = {}
-    for j in range(len(categories)):
-        conteggi[j] = 0
-    for x in reuter_docs:
-        y = open(x, 'r')
-        io = y.readlines()
-        #t = time.time()
-        print("File "+ y.name+ " in lettura")
-        for line in io:
-            for j in nomi.keys():
-                if '<D>' + str(nomi.get(j)).rstrip("']").lstrip("['") + '</D>' in line:
-                    conteggi[j] = conteggi[j] + 1
-        y.close()
-        #tf = time.time() - t
-        #print('Ricerca completata: exec time : %0.3fs' % tf)
-    #print(str(conteggi))
-    sorted_top10_categories = {}
-    count = 0
-    while count != 10:
-        maxKey = 0
-        maxValue = 0
-        for key, value in conteggi.items():
-            if value >= maxValue:
-                maxKey = key
-                maxValue = value
-        count += 1
-        del conteggi[maxKey]
-        sorted_top10_categories.__setitem__(maxKey, maxValue)
-    #print('Dizionario delle dieci categorie più frequenti: ' + str(sorted_top10_categories))
-    listaNomiCategorieTop10 = []
-    for key, value in nomi.items():
-        if key in sorted_top10_categories.keys():
-            listaNomiCategorieTop10.append(value)
-
-    #Cacho la lista
+        print("Ma non esiste, quindi procedo a trovarla")
+        
+# Cacho la lista
     with open("cacheTop10.txt", 'w') as f:
         for s in listaNomiCategorieTop10:
             f.write(str(s).replace("[", "").replace("]", "").replace("'", "") + '\n')
 
-    return listaNomiCategorieTop10
+'''
+import os
+import Preprocessing
 
-#TODO
-#categorie_freq()
+''' ReutersLoadFiles loads all files from reuters21578 folder and it splits them in two main subfolders:
+        -training-set
+        -test-set
 
-X_train_reuters = []
-y_train_reuters = []
-X_test_reuters = []
-y_test_reuters = []
-for x in reuter_docs: #"reuters21578/reut2-000.sgm"#for x in docfiles:
-    top10categorie = categorie_freq(use_cache=True)
-    io = open(x, 'r')
-    strr = io.read()
-    docs = strr.split("</REUTERS>")
-    for sample in docs:
-        if any('<D>' + str(y).rstrip("']").lstrip("['") + '</D>' in sample for y in top10categorie):
-            #if not os.path.exists(current_working_folder + "/data-set"):
-                #os.makedirs(current_working_folder + "/data-set")
-            if 'LEWISSPLIT="TRAIN"' in sample and 'TOPICS="YES"' in sample:
-                #if not os.path.exists(current_working_folder + "/data-set/training-set"):
-                    #os.makedirs(current_working_folder + "/data-set/training-set")
-                for y in top10categorie:
-                    if '<D>' + str(y).rstrip("']").lstrip("['") + '</D>' in sample:
-                        #if not os.path.exists(current_working_folder + "/data-set/training-set/" + str(y).rstrip("']").lstrip("['")):
-                        #    os.makedirs(current_working_folder + "/data-set/training-set/" + str(y).rstrip("']").lstrip("['"))
-                        #os.chdir(current_working_folder + "/data-set/training-set/" + str(y).rstrip("']").lstrip("['"))
-                        indexTitle = sample.index("NEWID=")
-                        endTitle = sample.index('">')
-                        title = '0' + sample[indexTitle + 7:endTitle]
-                        body = body_extractor(sample)
-                        X_train_reuters.append(body)
-                        y_train_reuters.append(str(y))
-            elif 'LEWISSPLIT="TEST"' in sample and 'TOPICS="YES"' in sample:
-                #if not os.path.exists(current_working_folder + "/data-set/test-set"):
-                #    os.makedirs(current_working_folder + "/data-set/test-set")
-                for y in top10categorie:
-                    if '<D>' + str(y).rstrip("']").lstrip("['") + '</D>' in sample:
-                        #if not os.path.exists(current_working_folder + "/data-set/test-set/" + str(y).rstrip("']").lstrip("['")):
-                        #    os.makedirs(current_working_folder + "/data-set/test-set/" + str(y).rstrip("']").lstrip("['"))
-                        #os.chdir(current_working_folder + "/data-set/test-set/" + str(y).rstrip("']").lstrip("['"))
-                        indexTitle = sample.index("NEWID=")
-                        endTitle = sample.index('">')
-                        title = '0' + sample[indexTitle + 7:endTitle]
-                        body = body_extractor(sample)
-                        X_test_reuters.append(body)
-                        y_test_reuters.append(str(y))
+    It uses Preprocessing.top10categories(...) to extract the 10 more frequent categories among all samples.
+
+    Please ensure that everytime this module is run, files and folders that were generated by previous executions must be deleted.
+
+        '''
+
+
+def Reuters_load_files():
+    current_working_folder = os.getcwd()
+    docfiles = [current_working_folder + '/reuters21578/reut2-000.sgm',
+                current_working_folder + '/reuters21578/reut2-001.sgm',
+                current_working_folder + '/reuters21578/reut2-002.sgm',
+                current_working_folder + '/reuters21578/reut2-003.sgm',
+                current_working_folder + '/reuters21578/reut2-004.sgm',
+                current_working_folder + '/reuters21578/reut2-005.sgm',
+                current_working_folder + '/reuters21578/reut2-006.sgm',
+                current_working_folder + '/reuters21578/reut2-007.sgm',
+                current_working_folder + '/reuters21578/reut2-008.sgm',
+                current_working_folder + '/reuters21578/reut2-009.sgm',
+                current_working_folder + '/reuters21578/reut2-010.sgm',
+                current_working_folder + '/reuters21578/reut2-011.sgm',
+                current_working_folder + '/reuters21578/reut2-012.sgm',
+                current_working_folder + '/reuters21578/reut2-013.sgm',
+                current_working_folder + '/reuters21578/reut2-014.sgm',
+                current_working_folder + '/reuters21578/reut2-015.sgm',
+                current_working_folder + '/reuters21578/reut2-016.sgm',
+                current_working_folder + '/reuters21578/reut2-018.sgm',
+                current_working_folder + '/reuters21578/reut2-019.sgm',
+                current_working_folder + '/reuters21578/reut2-020.sgm',
+                current_working_folder + '/reuters21578/reut2-021.sgm']
+    print(
+        'Analizzo i file per controllare quali sono le 10 categorie più frequenti:(potrebbe richieder qualche minuto)')
+
+    listaNomiCategorieTop10 = Preprocessing.top10categories(current_working_folder, docfiles)
+
+    print('lista dei tag riferiti alle 10 categorie più frequenti:')
+    for y in listaNomiCategorieTop10:
+        print('<D>' + str(y).rstrip("']").lstrip("['") + '</D>')
+
+    print('Orgnizzo file in cartelle:')
+
+    for x in docfiles:
+        io = open(x, 'r')
+        strr = io.read()
+        docs = strr.split("</REUTERS>")
+        for sample in docs:
+            if any('<D>' + str(y).rstrip("']").lstrip("['") + '</D>' in sample for y in listaNomiCategorieTop10):
+                if not os.path.exists(current_working_folder + "/data-set"):
+                    os.makedirs(current_working_folder + "/data-set")
+                if 'LEWISSPLIT="TRAIN"' in sample and 'TOPICS="YES"' in sample:
+                    if not os.path.exists(current_working_folder + "/data-set/training-set"):
+                        os.makedirs(current_working_folder + "/data-set/training-set")
+                    for y in listaNomiCategorieTop10:
+                        if '<D>' + str(y).rstrip("']").lstrip("['") + '</D>' in sample:
+                            if not os.path.exists(current_working_folder + "/data-set/training-set/" + str(y).rstrip(
+                                    "']").lstrip("['")):
+                                os.makedirs(
+                                    current_working_folder + "/data-set/training-set/" + str(y).rstrip("']").lstrip(
+                                        "['"))
+                            os.chdir(
+                                current_working_folder + "/data-set/training-set/" + str(y).rstrip("']").lstrip("['"))
+                            indexTitle = sample.index("NEWID=")
+                            endTitle = sample.index('">')
+                            title = '0' + sample[indexTitle + 7:endTitle]
+                            body = Preprocessing.body_extractor(sample)
+                            output = open(title + '.txt', 'w')
+                            output.write(sample)
+                elif 'LEWISSPLIT="TEST"' in sample and 'TOPICS="YES"' in sample:
+                    if not os.path.exists(current_working_folder + "/data-set/test-set"):
+                        os.makedirs(current_working_folder + "/data-set/test-set")
+                    for y in listaNomiCategorieTop10:
+                        if '<D>' + str(y).rstrip("']").lstrip("['") + '</D>' in sample:
+                            if not os.path.exists(current_working_folder + "/data-set/test-set/" + str(y).rstrip(
+                                    "']").lstrip("['")):
+                                os.makedirs(
+                                    current_working_folder + "/data-set/test-set/" + str(y).rstrip("']").lstrip("['"))
+                            os.chdir(current_working_folder + "/data-set/test-set/" + str(y).rstrip("']").lstrip("['"))
+                            indexTitle = sample.index("NEWID=")
+                            endTitle = sample.index('">')
+                            title = '0' + sample[indexTitle + 7:endTitle]
+                            body = Preprocessing.body_extractor(sample)
+                            output = open(title + '.txt', 'w')
+                            output.write(body)
     print('Organizzazione in cartelle completata.')
+
+
+
+def warmup():
+    if Path(r"data-set/").exists():
+        print("Dataset reuter presente")
+    else:
+        print("Dataset reuter NON presente")
+        Reuters_load_files()
+
+if __name__ == '__main__':
+    warmup()
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+from os import getcwd
+
 from nltk import word_tokenize
 from nltk.stem.porter import PorterStemmer
 import re
@@ -7,125 +9,62 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, T
 from sklearn.model_selection import learning_curve, ShuffleSplit, train_test_split
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.linear_model import Perceptron
-from sklearn.datasets import fetch_20newsgroups
+from sklearn.datasets import fetch_20newsgroups, load_files
 import nltk
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import metrics
 
+import DatasetRetriver
+import Reuters
 
-def plot_learning_curve(estimator, title, X, y, axes=None, ylim=None, cv=None,
-                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5)):
-    """
-    Generate 3 plots: the test and training learning curve, the training
-    samples vs fit times curve, the fit times vs score curve.
 
-    Parameters
-    ----------
-    estimator : estimator instance
-        An estimator instance implementing `fit` and `predict` methods which
-        will be cloned for each validation.
+def show_top(classifier, vectorizer, categories):
+    feature_names = np.asarray(vectorizer.get_feature_names())
+    for i, category in enumerate(categories):
+        top10 = np.argsort(classifier.coef_[i])[-20:]
+        print("\x1b[31m %s \x1b[0m : %s"
+              % (category, ", ".join(feature_names[top10])))
 
-    title : str
-        Title for the chart.
 
-    X : array-like of shape (n_samples, n_features)
-        Training vector, where ``n_samples`` is the number of samples and
-        ``n_features`` is the number of features.
+def plot_learning_curves(estimator1, estimator2, title, x1, y, n_jobs=-1, train_sizes=np.linspace(.1, 1.0, 10)):
+    plt.figure()
+    plt.title(title)
+    plt.xlabel("Samples")
 
-    y : array-like of shape (n_samples) or (n_samples, n_features)
-        Target relative to ``X`` for classification or regression;
-        None for unsupervised learning.
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(estimator1, x1, y, cv=10, n_jobs=n_jobs,
+                                                            train_sizes=train_sizes)
+    train_sizes1, train_scores1, test_scores1 = learning_curve(estimator2, x1, y, cv=10, n_jobs=n_jobs,
+                                                               train_sizes=train_sizes)
 
-    axes : array-like of shape (3,), default=None
-        Axes to use for plotting the curves.
-
-    ylim : tuple of shape (2,), default=None
-        Defines minimum and maximum y-values plotted, e.g. (ymin, ymax).
-
-    cv : int, cross-validation generator or an iterable, default=None
-        Determines the cross-validation splitting strategy.
-        Possible inputs for cv are:
-
-          - None, to use the default 5-fold cross-validation,
-          - integer, to specify the number of folds.
-          - :term:`CV splitter`,
-          - An iterable yielding (train, test) splits as arrays of indices.
-
-        For integer/None inputs, if ``y`` is binary or multiclass,
-        :class:`StratifiedKFold` used. If the estimator is not a classifier
-        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
-
-        Refer :ref:`User Guide <cross_validation>` for the various
-        cross-validators that can be used here.
-
-    n_jobs : int or None, default=None
-        Number of jobs to run in parallel.
-        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
-        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
-        for more details.
-
-    train_sizes : array-like of shape (n_ticks,)
-        Relative or absolute numbers of training examples that will be used to
-        generate the learning curve. If the ``dtype`` is float, it is regarded
-        as a fraction of the maximum size of the training set (that is
-        determined by the selected validation method), i.e. it has to be within
-        (0, 1]. Otherwise it is interpreted as absolute sizes of the training
-        sets. Note that for classification the number of samples usually have
-        to be big enough to contain at least one sample from each class.
-        (default: np.linspace(0.1, 1.0, 5))
-    """
-    if axes is None:
-        _, axes = plt.subplots(1, 3, figsize=(20, 5))
-
-    axes[0].set_title(title)
-    if ylim is not None:
-        axes[0].set_ylim(*ylim)
-    axes[0].set_xlabel("Training examples")
-    axes[0].set_ylabel("Score")
-
-    train_sizes, train_scores, test_scores, fit_times, _ = \
-        learning_curve(estimator, X, y, cv=cv, n_jobs=n_jobs,
-                       train_sizes=train_sizes,
-                       return_times=True)
+    # Computes average and standard deviation for the test score and the train score
     train_scores_mean = np.mean(train_scores, axis=1)
     train_scores_std = np.std(train_scores, axis=1)
     test_scores_mean = np.mean(test_scores, axis=1)
     test_scores_std = np.std(test_scores, axis=1)
-    fit_times_mean = np.mean(fit_times, axis=1)
-    fit_times_std = np.std(fit_times, axis=1)
 
-    # Plot learning curve
-    axes[0].grid()
-    axes[0].fill_between(train_sizes, train_scores_mean - train_scores_std,
-                         train_scores_mean + train_scores_std, alpha=0.1,
-                         color="r")
-    axes[0].fill_between(train_sizes, test_scores_mean - test_scores_std,
-                         test_scores_mean + test_scores_std, alpha=0.1,
-                         color="g")
-    axes[0].plot(train_sizes, train_scores_mean, 'o-', color="r",
-                 label="Training score")
-    axes[0].plot(train_sizes, test_scores_mean, 'o-', color="g",
-                 label="Cross-validation score")
-    axes[0].legend(loc="best")
+    train_scores_mean1 = np.mean(train_scores1, axis=1)
+    train_scores_std1 = np.std(train_scores1, axis=1)
+    test_scores_mean1 = np.mean(test_scores1, axis=1)
+    test_scores_std1 = np.std(test_scores1, axis=1)
+    plt.grid()
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="w")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="w")
 
-    # Plot n_samples vs fit_times
-    axes[1].grid()
-    axes[1].plot(train_sizes, fit_times_mean, 'o-')
-    axes[1].fill_between(train_sizes, fit_times_mean - fit_times_std,
-                         fit_times_mean + fit_times_std, alpha=0.1)
-    axes[1].set_xlabel("Training examples")
-    axes[1].set_ylabel("fit_times")
-    axes[1].set_title("Scalability of the model -" + title)
-
-    # Plot fit_time vs score
-    axes[2].grid()
-    axes[2].plot(fit_times_mean, test_scores_mean, 'o-')
-    axes[2].fill_between(fit_times_mean, test_scores_mean - test_scores_std,
-                         test_scores_mean + test_scores_std, alpha=0.1)
-    axes[2].set_xlabel("fit_times")
-    axes[2].set_ylabel("Score")
-    axes[2].set_title("Performance of the model -" + title)
+    plt.fill_between(train_sizes1, train_scores_mean1 - train_scores_std1,
+                     train_scores_mean1 + train_scores_std1, alpha=0.1,
+                     color="w")
+    plt.fill_between(train_sizes1, test_scores_mean1 - test_scores_std1,
+                     test_scores_mean1 + test_scores_std1, alpha=0.1, color="w")
+    plt.plot(train_sizes1, test_scores_mean1, 'o-', color="r",
+             label="Percy")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="BernoulliNB")
+    plt.legend(loc="best")
     return plt
 
 
@@ -180,7 +119,15 @@ if __name__ == '__main__':
     print("Completato")
     print("Reperimento Datasets: Reuters", end=" ")
     print("E chi ne ha voglia? Io? Ahahaha ti sbagli!!", end="...")
-    #Reuters
+    Reuters.warmup()
+    # X_train_reuters, X_test_reuters, y_train_reuters, y_test_reuters = Reuters.giveXyreuters()
+    import os
+
+    # cwd = os.getcwd()
+
+    X_train_reuters = load_files(container_path='./data-set/training-set', shuffle=True, random_state=42)
+    X_test_reuters = load_files(container_path='./data-set/test-set', shuffle=True, random_state=42)
+    # Reuters
     print("Completato")
     '''
     X, y = fetch_20newsgroups(return_X_y=True, shuffle=True, remove=remove)
@@ -202,60 +149,96 @@ if __name__ == '__main__':
     n = train_20news.target.shape[0]
     print(n, "training documents")
     '''
-    print("Vettorizzazione dei documenti", end=" ")
-    vectorizer = CountVectorizer(tokenizer=LemmaTokenizer(), stop_words="english", min_df=3)#, binary=True)
+    print("Vettorizzazione dei documenti")
+    vectorizer = CountVectorizer(tokenizer=LemmaTokenizer(), stop_words="english", min_df=3)  # , binary=True)
     TR_vectorized_20News = vectorizer.fit_transform(train_20news.data)
     TE_vectorized_20News = vectorizer.transform(test_20news.data)
-    print("Completata")
-    bnb = BernoulliNB()
-    prc = Perceptron()
+    print("20News Completato")
+    TR_vectorized_reuters = vectorizer.fit_transform(X_train_reuters.data)
+    TE_vectorized_reuters = vectorizer.transform(X_test_reuters.data)
+    print("Reuters Completato")
+    bnb20 = BernoulliNB()
+    prc20 = Perceptron()
 
     print("Addestramento (fitting) delle reti per 20News groups:")
     print("Bernoullian Naive Bayes", end=" ")
-    bnb.fit(TR_vectorized_20News, train_20news.target)
+    bnb20.fit(TR_vectorized_20News, train_20news.target)
     print("Completato")
     print("Perceptron", end=" ")
-    prc.fit(TR_vectorized_20News, train_20news.target)
+    prc20.fit(TR_vectorized_20News, train_20news.target)
     print("Completato")
 
-    '''
+    bnbre = BernoulliNB()
+    prcre = Perceptron()
     print("Addestramento (fitting) delle reti per Reuters:")
     print("Bernoullian Naive Bayes", end=" ")
-    bnb.fit(TR_vectorized_20News, train_20news.target)
+    bnbre.fit(TR_vectorized_reuters, X_train_reuters.target)
     print("Completato")
     print("Perceptron", end=" ")
-    prc.fit(TR_vectorized_20News, train_20news.target)
+    prcre.fit(TR_vectorized_reuters, X_train_reuters.target)
     print("Completato")
-    '''
-    predict = bnb.predict(TE_vectorized_20News)
-    predictPRC = prc.predict(TE_vectorized_20News)
 
-    print("Predizioni fatte")
+    predict_bnb20 = bnb20.predict(TE_vectorized_20News)
+    predict_prc20 = prc20.predict(TE_vectorized_20News)
+    print("Predizioni fatte per 20News group")
+    predict_bnbreut = bnbre.predict(TE_vectorized_reuters)
+    predict_prcreut = prcre.predict(TE_vectorized_reuters)
+
     print("Report Bernoullian Naive Bayes")
     print("20News groups: ")
-    report(test_20news.target, predict, train_20news.target_names)
+    report(test_20news.target, predict_bnb20, train_20news.target_names)
     print("Reuters")
-    #report
+    report(X_test_reuters.target, predict_bnbreut, X_train_reuters.target_names)
+
+    # report
 
     print("Report Perceptron")
     print("20News groups: ")
-    report(test_20news.target, predictPRC, train_20news.target_names)
+    report(test_20news.target, predict_prc20, train_20news.target_names)
     print("Reuters")
-    #report
+    report(X_test_reuters.target, predict_prcreut, X_test_reuters.target_names)
+
+    # report
 
     print("Generazione grafici")
-    cv = ShuffleSplit(n_splits=100, test_size=0.2)#, random_state=0)
+    cv = ShuffleSplit(n_splits=100, test_size=0.2)  # , random_state=0)
     train_sizes = np.logspace(np.log10(.1), np.log10(1.0), 8)
-    train_sizes, train_scores, test_scores = learning_curve(bnb, TR_vectorized_20News, train_20news.target, cv=cv,
-                                                            scoring="f1_weighted", train_sizes=train_sizes, n_jobs=-1)
-    plot_curve(train_sizes, train_scores, test_scores, title="Bernoulli Naive Bayes")
+    '''
+    Title1 = "Learning Curves on 20newsgroups"
+    Title2 = "Learning Curves on Reuters-21578"
 
-    train_sizes, train_scores, test_scores = learning_curve(prc, TR_vectorized_20News, train_20news.target, cv=cv,
-                                                            scoring="f1_weighted", train_sizes=train_sizes, n_jobs=-1)
-    plot_curve(train_sizes, train_scores, test_scores, title="Perceptron")
+    plot_learning_curves(bnb, prc, Title1, TR_vectorized_20News, train_20news.target,
+                                    n_jobs=1,
+                                    train_sizes=([0.01, 0.1, 0.2, 0.3, 0.4, 0.6, 0.7, 0.8, 0.9, 1.]))
     plt.show()
+    plt.savefig('grafico1.png', bbox_inches='tight')
+
+    plot_learning_curves(bnb, prc, Title2, TR_vectorized_reut,
+                                    X_reuters.target, n_jobs=1,
+                                    train_sizes=([0.01, 0.1, 0.2, 0.3, 0.4, 0.6, 0.7, 0.8, 0.9, 1.]))
+    plt.show()
+    plt.savefig('grafico2.png', bbox_inches='tight')
+    '''
+    train_sizes, train_scores, test_scores = learning_curve(bnb20, TR_vectorized_20News, train_20news.target, cv=cv,
+                                                            scoring="f1_weighted", train_sizes=train_sizes, n_jobs=-1)
+    plot_curve(train_sizes, train_scores, test_scores, title="Bernoulli Naive Bayes 20Group")
+
+    train_sizes, train_scores, test_scores = learning_curve(prc20, TR_vectorized_20News, train_20news.target, cv=cv,
+                                                            scoring="f1_weighted", train_sizes=train_sizes, n_jobs=-1)
+    plot_curve(train_sizes, train_scores, test_scores, title="Perceptron 20Group")
+##############################################################################################
+    train_sizes, train_scores, test_scores = learning_curve(bnbre, TR_vectorized_reuters, X_train_reuters.target, cv=cv,
+                                                            scoring="f1_weighted", train_sizes=train_sizes, n_jobs=-1)
+    plot_curve(train_sizes, train_scores, test_scores, title="Bernoulli Naive Bayes reuters")
+
+    train_sizes, train_scores, test_scores = learning_curve(prcre, TR_vectorized_reuters, X_train_reuters.target, cv=cv,
+                                                            scoring="f1_weighted", train_sizes=train_sizes, n_jobs=-1)
+    plot_curve(train_sizes, train_scores, test_scores, title="Perceptron reuters")
+    plt.show()
+    plt.savefig('grafici.png')  # , bbox_inches='tight')
 
     print("Fine")
+
     '''
     fig, axes = plt.subplots(3, 2, figsize=(10, 15))
 
