9f615ae385272def7cea0413e0b0a245423e446c
Ciroso <arman.lagevardi@hotmail.com>
2021-06-25 20:18:23 +0200

    Variazioni sul metodo di richiamo dei dataset per entrambi

--- main.py
+++ main.py
@@ -1,44 +1,35 @@
-# This is a sample Python script.
-from os import getcwd
-
 from nltk import word_tokenize
 from nltk.stem.porter import PorterStemmer
 import re
 import string
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
-from sklearn.model_selection import learning_curve, ShuffleSplit, train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import learning_curve, ShuffleSplit
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.linear_model import Perceptron
 from sklearn.datasets import fetch_20newsgroups, load_files
-import nltk
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import metrics
-
-import DatasetRetriver
 import Reuters
 
 
-def show_top(classifier, vectorizer, categories):
-    feature_names = np.asarray(vectorizer.get_feature_names())
-    for i, category in enumerate(categories):
-        top10 = np.argsort(classifier.coef_[i])[-20:]
-        print("\x1b[31m %s \x1b[0m : %s"
-              % (category, ", ".join(feature_names[top10])))
+def plot_learning_curves(estimator1, estimator2, title, x, y):
+    n_jobs = -1
+    train_sizes = np.linspace(.1, 1.0, 10)
+    # train_sizes = np.logspace(np.log10(.1), np.log10(1.0), 8)
+    cv = ShuffleSplit(n_splits=100, test_size=0.2)
 
-
-def plot_learning_curves(estimator1, estimator2, title, x1, y, n_jobs=-1, train_sizes=np.linspace(.1, 1.0, 10)):
     plt.figure()
     plt.title(title)
-    plt.xlabel("Samples")
+    plt.xlabel("Numero di Esempi")
 
     plt.ylabel("Score")
-    train_sizes, train_scores, test_scores = learning_curve(estimator1, x1, y, cv=10, n_jobs=n_jobs,
+    train_sizes, train_scores, test_scores = learning_curve(estimator1, x, y, cv=cv, n_jobs=n_jobs,
                                                             train_sizes=train_sizes)
-    train_sizes1, train_scores1, test_scores1 = learning_curve(estimator2, x1, y, cv=10, n_jobs=n_jobs,
+    train_sizes1, train_scores1, test_scores1 = learning_curve(estimator2, x, y, cv=cv, n_jobs=n_jobs,
                                                                train_sizes=train_sizes)
 
-    # Computes average and standard deviation for the test score and the train score
+    # Calcolo la media degli attributi dello score
     train_scores_mean = np.mean(train_scores, axis=1)
     train_scores_std = np.std(train_scores, axis=1)
     test_scores_mean = np.mean(test_scores, axis=1)
@@ -49,21 +40,15 @@ def plot_learning_curves(estimator1, estimator2, title, x1, y, n_jobs=-1, train_
     test_scores_mean1 = np.mean(test_scores1, axis=1)
     test_scores_std1 = np.std(test_scores1, axis=1)
     plt.grid()
-    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
-                     train_scores_mean + train_scores_std, alpha=0.1,
-                     color="w")
-    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
-                     test_scores_mean + test_scores_std, alpha=0.1, color="w")
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, color="w")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, color="w")
 
-    plt.fill_between(train_sizes1, train_scores_mean1 - train_scores_std1,
-                     train_scores_mean1 + train_scores_std1, alpha=0.1,
+    plt.fill_between(train_sizes1, train_scores_mean1 - train_scores_std1, train_scores_mean1 + train_scores_std1,
                      color="w")
-    plt.fill_between(train_sizes1, test_scores_mean1 - test_scores_std1,
-                     test_scores_mean1 + test_scores_std1, alpha=0.1, color="w")
-    plt.plot(train_sizes1, test_scores_mean1, 'o-', color="r",
-             label="Percy")
-    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
-             label="BernoulliNB")
+    plt.fill_between(train_sizes1, test_scores_mean1 - test_scores_std1, test_scores_mean1 + test_scores_std1,
+                     color="w")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g", label="BernoulliNB")
+    plt.plot(train_sizes1, test_scores_mean1, 'o-', color="r", label="Perceptron")
     plt.legend(loc="best")
     return plt
 
@@ -78,125 +63,74 @@ class LemmaTokenizer(object):
         return [stemmer.stem(t) for t in clean if len(t) > 2]
 
 
-def plot_curve(train_sizes, train_scores, test_scores, title='', ylim=None):
-    plt.figure(figsize=(8, 6))
-    plt.title(title)
-    if ylim is not None:
-        plt.ylim(*ylim)
-    plt.xlabel("Training examples")
-    plt.ylabel("F1 weighted score")
-    plt.grid(True)
-    train_scores_mean = np.mean(train_scores, axis=1)
-    train_scores_std = np.std(train_scores, axis=1)
-    test_scores_mean = np.mean(test_scores, axis=1)
-    test_scores_std = np.std(test_scores, axis=1)
-    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
-                     train_scores_mean + train_scores_std, alpha=0.1,
-                     color="r")
-    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
-                     test_scores_mean + test_scores_std,
-                     alpha=0.1, color="g")
-    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
-             label="Training set")
-    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
-             label="Validation set")
-    plt.legend(loc="best")
-
-
-def report(target, pred, target_names):
-    cm = metrics.confusion_matrix(target, pred)
-    print('Contingency matrix: (rows: examples of a given true class)')
-    print(cm)
-    print('Classification report')
-    print(metrics.classification_report(target, pred, target_names=target_names))
-
-
 if __name__ == '__main__':
     remove = ('headers', 'footers', 'quotes')
     print("Reperimento Datasets: 20News groups", end=" ")
-    train_20news = fetch_20newsgroups(subset='train', shuffle=True, remove=remove)  # , categories=categories)
-    test_20news = fetch_20newsgroups(subset='test', shuffle=True, remove=remove)  # , categories=categories)
+    # train_20news = fetch_20newsgroups(subset='train', shuffle=True, remove=remove)  # , categories=categories)
+    # test_20news = fetch_20newsgroups(subset='test', shuffle=True, remove=remove)  # , categories=categories)
+    X_20news, y_20news = fetch_20newsgroups(return_X_y=True, shuffle=True, remove=remove)
     print("Completato")
+
     print("Reperimento Datasets: Reuters", end=" ")
-    print("E chi ne ha voglia? Io? Ahahaha ti sbagli!!", end="...")
-    Reuters.warmup()
-    # X_train_reuters, X_test_reuters, y_train_reuters, y_test_reuters = Reuters.giveXyreuters()
-    import os
-
-    # cwd = os.getcwd()
-
-    X_train_reuters = load_files(container_path='./data-set/training-set', shuffle=True, random_state=42)
-    X_test_reuters = load_files(container_path='./data-set/test-set', shuffle=True, random_state=42)
-    # Reuters
+    #Reuters.warmup()
+    #X_train_reuters = load_files(container_path='data-set/training-set', shuffle=True, random_state=42)
+    #X_test_reuters = load_files(container_path='data-set/test-set', shuffle=True, random_state=42)
+    X_reut, y_reut = Reuters.return_X_y_reut()
     print("Completato")
-    '''
-    X, y = fetch_20newsgroups(return_X_y=True, shuffle=True, remove=remove)
-    bnb = BernoulliNB()
-    prc = Perceptron()
-    vectorizer = CountVectorizer(tokenizer=LemmaTokenizer(), stop_words="english", min_df=3)#, binary=True)
-    Vect_X = vectorizer.fit_transform(X)
-    TR_X, TE_X = train_test_split(Vect_X, shuffle=False)
-    TR_y, TE_y = train_test_split(y, shuffle=False)
-    bnb.fit(TR_X, TR_y)
-    prc.fit(TR_X, TR_y)
-    predict = bnb.predict(TE_X)
-    predictPRC = prc.predict(TE_X)
-    report(TE_y, predict, train_20news.target_names)
-    report(TE_y, predictPRC, train_20news.target_names)
 
-    print(train_20news.target_names)
-    print(test_20news.target_names)
-    n = train_20news.target.shape[0]
-    print(n, "training documents")
-    '''
     print("Vettorizzazione dei documenti")
     vectorizer = CountVectorizer(tokenizer=LemmaTokenizer(), stop_words="english", min_df=3)  # , binary=True)
-    TR_vectorized_20News = vectorizer.fit_transform(train_20news.data)
-    TE_vectorized_20News = vectorizer.transform(test_20news.data)
+    #X_vectorized_20news = vectorizer.fit_transform(X_20news)
+    # TR_vectorized_20News = vectorizer.fit_transform(train_20news.data)
+    # TE_vectorized_20News = vectorizer.transform(test_20news.data)
     print("20News Completato")
-    TR_vectorized_reuters = vectorizer.fit_transform(X_train_reuters.data)
-    TE_vectorized_reuters = vectorizer.transform(X_test_reuters.data)
+    X_vectorized_reut = vectorizer.fit_transform(X_reut)
+    # TR_vectorized_reuters = vectorizer.fit_transform(X_train_reuters.data)
+    # TE_vectorized_reuters = vectorizer.transform(X_test_reuters.data)
     print("Reuters Completato")
+
     bnb20 = BernoulliNB()
     prc20 = Perceptron()
-
     print("Addestramento (fitting) delle reti per 20News groups:")
     print("Bernoullian Naive Bayes", end=" ")
-    bnb20.fit(TR_vectorized_20News, train_20news.target)
+    # bnb20.fit(TR_vectorized_20News, train_20news.target)
     print("Completato")
     print("Perceptron", end=" ")
-    prc20.fit(TR_vectorized_20News, train_20news.target)
+    # prc20.fit(TR_vectorized_20News, train_20news.target)
     print("Completato")
 
     bnbre = BernoulliNB()
     prcre = Perceptron()
     print("Addestramento (fitting) delle reti per Reuters:")
     print("Bernoullian Naive Bayes", end=" ")
-    bnbre.fit(TR_vectorized_reuters, X_train_reuters.target)
+    # bnbre.fit(TR_vectorized_reuters, X_train_reuters.target)
     print("Completato")
     print("Perceptron", end=" ")
-    prcre.fit(TR_vectorized_reuters, X_train_reuters.target)
+    # prcre.fit(TR_vectorized_reuters, X_train_reuters.target)
     print("Completato")
 
-    predict_bnb20 = bnb20.predict(TE_vectorized_20News)
-    predict_prc20 = prc20.predict(TE_vectorized_20News)
+    print("Calcolo delle predizioni per 20News group")
+    # predict_bnb20 = bnb20.predict(TE_vectorized_20News)
+    # predict_prc20 = prc20.predict(TE_vectorized_20News)
     print("Predizioni fatte per 20News group")
-    predict_bnbreut = bnbre.predict(TE_vectorized_reuters)
-    predict_prcreut = prcre.predict(TE_vectorized_reuters)
+    print("Calcolo delle predizioni per Reuters")
+    # predict_bnbreut = bnbre.predict(TE_vectorized_reuters)
+    # predict_prcreut = prcre.predict(TE_vectorized_reuters)
+    print("Predizioni fatte per Reuters")
 
     print("Report Bernoullian Naive Bayes")
     print("20News groups: ")
-    report(test_20news.target, predict_bnb20, train_20news.target_names)
-    print("Reuters")
-    report(X_test_reuters.target, predict_bnbreut, X_train_reuters.target_names)
+    # report(test_20news.target, predict_bnb20, train_20news.target_names)
+    print("Reuters: ")
+    # report(X_test_reuters.target, predict_bnbreut, X_train_reuters.target_names)
 
     # report
 
     print("Report Perceptron")
     print("20News groups: ")
-    report(test_20news.target, predict_prc20, train_20news.target_names)
-    print("Reuters")
-    report(X_test_reuters.target, predict_prcreut, X_test_reuters.target_names)
+    # report(test_20news.target, predict_prc20, train_20news.target_names)
+    print("Reuters: ")
+    # report(X_test_reuters.target, predict_prcreut, X_test_reuters.target_names)
 
     # report
 
@@ -219,18 +153,26 @@ if __name__ == '__main__':
     plt.show()
     plt.savefig('grafico2.png', bbox_inches='tight')
     '''
+    '''
     train_sizes, train_scores, test_scores = learning_curve(bnb20, TR_vectorized_20News, train_20news.target, cv=cv,
                                                             scoring="f1_weighted", train_sizes=train_sizes, n_jobs=-1)
     plot_curve(train_sizes, train_scores, test_scores, title="Bernoulli Naive Bayes 20Group")
+    plt.show()
 
+    train_sizes = np.logspace(np.log10(.1), np.log10(1.0), 8)
     train_sizes, train_scores, test_scores = learning_curve(prc20, TR_vectorized_20News, train_20news.target, cv=cv,
                                                             scoring="f1_weighted", train_sizes=train_sizes, n_jobs=-1)
     plot_curve(train_sizes, train_scores, test_scores, title="Perceptron 20Group")
+    plt.show()
+
 ##############################################################################################
+    train_sizes = np.logspace(np.log10(.1), np.log10(1.0), 8)
     train_sizes, train_scores, test_scores = learning_curve(bnbre, TR_vectorized_reuters, X_train_reuters.target, cv=cv,
                                                             scoring="f1_weighted", train_sizes=train_sizes, n_jobs=-1)
     plot_curve(train_sizes, train_scores, test_scores, title="Bernoulli Naive Bayes reuters")
+    plt.show()
 
+    train_sizes = np.logspace(np.log10(.1), np.log10(1.0), 8)
     train_sizes, train_scores, test_scores = learning_curve(prcre, TR_vectorized_reuters, X_train_reuters.target, cv=cv,
                                                             scoring="f1_weighted", train_sizes=train_sizes, n_jobs=-1)
     plot_curve(train_sizes, train_scores, test_scores, title="Perceptron reuters")
@@ -238,6 +180,21 @@ if __name__ == '__main__':
     plt.savefig('grafici.png')  # , bbox_inches='tight')
 
     print("Fine")
+    '''
+    Title1 = "Learning Curves on 20newsgroups"
+    Title2 = "Learning Curves on Reuters-21578"
+
+    # plot_learning_curves(bnb20, prc20, Title1, TR_vectorized_20News, train_20news.target)
+    #plot_learning_curves(bnb20, prc20, Title1, X_vectorized_20news, y_20news)
+    #plt.savefig("bnb_prc_20group.png", dpi=100)
+    #plt.show()
+    #plt.savefig("primo.jpg")
+    # plot_learning_curves(bnbre, prcre, Title2, TR_vectorized_reuters, X_train_reuters.target)
+    #X_vectorized_reut = TR_vectorized_reuters
+    #y_reut = X_train_reuters.target
+    plot_learning_curves(bnbre, prcre, Title2, X_vectorized_reut, y_reut)
+    plt.savefig("bnb_prc_Reuters.png",dpi = 100)
+    plt.show()
 
     '''
     fig, axes = plt.subplots(3, 2, figsize=(10, 15))
