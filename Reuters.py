296a03152ab0d0527f3aa3ecdead303f96a413ca
Ciroso <arman.lagevardi@hotmail.com>
2021-06-26 00:00:44 +0200

    Final commit

--- Reuters.py
+++ Reuters.py
@@ -1,18 +1,17 @@
 from pathlib import Path
 from bs4 import BeautifulSoup
-import codecs
 import os
 
 
 def top10categories(reuters_files, use_cache=True):
     if use_cache:
-        print("Utilizzo la cache di top10categorie reuters")
+        print("Utilizzo la cache di top10categories Reuters", end=" ")
         if Path(r"cacheTop10.txt").exists():
             with open("cacheTop10.txt", 'r') as f:
                 top10 = [line.rstrip('\n') for line in f]
             return top10
         else:
-            print("Ma non esiste, quindi procedo a trovarla")
+            print("ma non esiste, quindi procedo a trovarla...", end=" ")
     file = open("reuters21578/all-topics-strings.lc.txt", "r")
     categories = []
     parts = []
@@ -23,7 +22,7 @@ def top10categories(reuters_files, use_cache=True):
         categories.append(parts)
     file.close()
     print("Inizio conteggio delle ricorrenze")
-    # CONTO QUANTE OCCORRENZE DI CIASCUNA CATEGORIA CI SONO#
+    # Conto quante occorrenze ci sono
     nomi = {}
     for i in range(len(categories)):
         nomi[i] = categories[i]
@@ -31,11 +30,11 @@ def top10categories(reuters_files, use_cache=True):
     for j in range(len(categories)):
         conteggi[j] = 0
     for x in reuters_files:
-        y = open(x, 'r')
+        y = open(x, "r")
         io = y.readlines()
         for line in io:
             for j in nomi.keys():
-                if '<D>' + str(nomi.get(j)).rstrip("']").lstrip("['") + '</D>' in line:
+                if "<D>" + str(nomi.get(j)).rstrip("']").lstrip("['") + "</D>" in line:
                     conteggi[j] = conteggi[j] + 1
         y.close()
     sorted_top10_categories = {}
@@ -60,92 +59,6 @@ def top10categories(reuters_files, use_cache=True):
     return Top10
 
 
-def Reuters_load_files():
-    cwf = os.getcwd()
-    reut_files = []
-    for i in range(0, 22):
-        if i < 10:
-            reut_files.append(cwf + "/reuters21578/reut2-00" + str(i) + ".sgm")
-        else:
-            reut_files.append(cwf + "/reuters21578/reut2-0" + str(i) + ".sgm")
-    '''
-    docfiles = [current_working_folder + '/reuters21578/reut2-000.sgm',
-                current_working_folder + '/reuters21578/reut2-001.sgm',
-                current_working_folder + '/reuters21578/reut2-002.sgm',
-                current_working_folder + '/reuters21578/reut2-003.sgm',
-                current_working_folder + '/reuters21578/reut2-004.sgm',
-                current_working_folder + '/reuters21578/reut2-005.sgm',
-                current_working_folder + '/reuters21578/reut2-006.sgm',
-                current_working_folder + '/reuters21578/reut2-007.sgm',
-                current_working_folder + '/reuters21578/reut2-008.sgm',
-                current_working_folder + '/reuters21578/reut2-009.sgm',
-                current_working_folder + '/reuters21578/reut2-010.sgm',
-                current_working_folder + '/reuters21578/reut2-011.sgm',
-                current_working_folder + '/reuters21578/reut2-012.sgm',
-                current_working_folder + '/reuters21578/reut2-013.sgm',
-                current_working_folder + '/reuters21578/reut2-014.sgm',
-                current_working_folder + '/reuters21578/reut2-015.sgm',
-                current_working_folder + '/reuters21578/reut2-016.sgm',
-                current_working_folder + '/reuters21578/reut2-017.sgm',
-                current_working_folder + '/reuters21578/reut2-018.sgm',
-                current_working_folder + '/reuters21578/reut2-019.sgm',
-                current_working_folder + '/reuters21578/reut2-020.sgm',
-                current_working_folder + '/reuters21578/reut2-021.sgm']
-    '''
-    listaNomiCategorieTop10 = top10categories(reut_files)
-
-    """print('lista dei tag riferiti alle 10 categorie più frequenti:')
-    for y in listaNomiCategorieTop10:
-        print('<D>' + str(y).rstrip("']").lstrip("['") + '</D>')
-    """
-    print('Orgnizzo file in cartelle')
-
-    for x in reut_files:
-        io = open(x, 'r')
-        strr = io.read()
-        docs = strr.split("</REUTERS>")
-        for sample in docs:
-            if any('<D>' + str(y).rstrip("']").lstrip("['") + '</D>' in sample for y in listaNomiCategorieTop10):
-                if not os.path.exists(cwf + "/data-set"):
-                    os.makedirs(cwf + "/data-set")
-                if 'LEWISSPLIT="TRAIN"' in sample and 'TOPICS="YES"' in sample:
-                    if not os.path.exists(cwf + "/data-set/training-set"):
-                        os.makedirs(cwf + "/data-set/training-set")
-                    for y in listaNomiCategorieTop10:
-                        if '<D>' + str(y).rstrip("']").lstrip("['") + '</D>' in sample:
-                            if not os.path.exists(cwf + "/data-set/training-set/" + str(y).rstrip("']").lstrip("['")):
-                                os.makedirs(cwf + "/data-set/training-set/" + str(y).rstrip("']").lstrip("['"))
-                            os.chdir(cwf + "/data-set/training-set/" + str(y).rstrip("']").lstrip("['"))
-                            indexTitle = sample.index("NEWID=")
-                            endTitle = sample.index('">')
-                            title = '0' + sample[indexTitle + 7:endTitle]
-                            soup = BeautifulSoup(sample, features="html.parser")
-                            reut_body = soup.findAll("body")
-                            if len(reut_body) != 0:
-                                body = str(reut_body[0].string)
-                                output = codecs.open(title + '.txt', 'w', "utf-8")
-                                output.write(body)
-
-                elif 'LEWISSPLIT="TEST"' in sample and 'TOPICS="YES"' in sample:
-                    if not os.path.exists(cwf + "/data-set/test-set"):
-                        os.makedirs(cwf + "/data-set/test-set")
-                    for y in listaNomiCategorieTop10:
-                        if '<D>' + str(y).rstrip("']").lstrip("['") + '</D>' in sample:
-                            if not os.path.exists(cwf + "/data-set/test-set/" + str(y).rstrip("']").lstrip("['")):
-                                os.makedirs(cwf + "/data-set/test-set/" + str(y).rstrip("']").lstrip("['"))
-                            os.chdir(cwf + "/data-set/test-set/" + str(y).rstrip("']").lstrip("['"))
-                            indexTitle = sample.index("NEWID=")
-                            endTitle = sample.index('">')
-                            title = '0' + sample[indexTitle + 7:endTitle]
-                            soup = BeautifulSoup(sample, features="html.parser")
-                            reut_body = soup.findAll("body")
-                            if len(reut_body) != 0:
-                                body = str(reut_body[0].string)
-                                output = codecs.open(title + '.txt', 'w', "utf-8")
-                                output.write(body)
-    print('Organizzazione in cartelle completata.')
-
-
 def return_X_y_reut():
     cwf = os.getcwd()
     reut_files = []
@@ -155,47 +68,22 @@ def return_X_y_reut():
         else:
             reut_files.append(cwf + "/reuters21578/reut2-0" + str(i) + ".sgm")
 
-    listaNomiCategorieTop10 = top10categories(reut_files)
-
-    # print('Orgnizzo file in cartelle')
+    top10cat = top10categories(reut_files)
     X = []
-    y_target = []
+    y = []
     for x in reut_files:
-        io = open(x, 'r')
+        io = open(x, "r")
         strr = io.read()
         docs = strr.split("</REUTERS>")
-        for sample in docs:
-            if any('<D>' + str(y).rstrip("']").lstrip("['") + '</D>' in sample for y in listaNomiCategorieTop10):
-                if 'LEWISSPLIT="TRAIN"' in sample and 'TOPICS="YES"' in sample:
-                    for y in listaNomiCategorieTop10:
-                        if '<D>' + str(y).rstrip("']").lstrip("['") + '</D>' in sample:
-                            soup = BeautifulSoup(sample, features="html.parser")
+        for segment in docs:
+            if any("<D>" + str(cat).rstrip("']").lstrip("['") + "</D>" in segment for cat in top10cat):
+                if ('LEWISSPLIT="TRAIN"' or 'LEWISSPLIT="TEST"') in segment and 'TOPICS="YES"' in segment:
+                    for cate in top10cat:
+                        if "<D>" + str(cate).rstrip("']").lstrip("['") + "</D>" in segment:
+                            soup = BeautifulSoup(segment, features="html.parser")
                             reut_body = soup.findAll("body")
                             if len(reut_body) != 0:
                                 body = str(reut_body[0].string)
                                 X.append(body)
-                                y_target.append(y)
-
-                elif 'LEWISSPLIT="TEST"' in sample and 'TOPICS="YES"' in sample:
-                    for y in listaNomiCategorieTop10:
-                        if '<D>' + str(y).rstrip("']").lstrip("['") + '</D>' in sample:
-                            soup = BeautifulSoup(sample, features="html.parser")
-                            reut_body = soup.findAll("body")
-                            if len(reut_body) != 0:
-                                body = str(reut_body[0].string)
-                                X.append(body)
-                                y_target.append(y)
-    print('Organizzazione in cartelle completata.')
-    return X, y_target
-
-
-def warmup():
-    if Path(r"data-set/").exists():
-        print("Dataset reuter presente")
-    else:
-        print("Dataset reuter NON presente")
-        Reuters_load_files()
-
-
-if __name__ == '__main__':
-    warmup()
+                                y.append(cate)
+    return X, y
--- main.py
+++ main.py
@@ -6,17 +6,15 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import learning_curve, ShuffleSplit
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.linear_model import Perceptron
-from sklearn.datasets import fetch_20newsgroups, load_files
+from sklearn.datasets import fetch_20newsgroups
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import metrics
 import Reuters
 
 
-def plot_learning_curves(estimator1, estimator2, title, x, y):
+def plot_learning_curves(bnb, prc, title, x, y):
     n_jobs = -1
-    train_sizes = np.linspace(.1, 1.0, 10)
-    # train_sizes = np.logspace(np.log10(.1), np.log10(1.0), 8)
+    train_sizes = np.logspace(np.log10(.1), np.log10(1.0), 8)
     cv = ShuffleSplit(n_splits=100, test_size=0.2)
 
     plt.figure()
@@ -24,31 +22,19 @@ def plot_learning_curves(estimator1, estimator2, title, x, y):
     plt.xlabel("Numero di Esempi")
 
     plt.ylabel("Score")
-    train_sizes, train_scores, test_scores = learning_curve(estimator1, x, y, cv=cv, n_jobs=n_jobs,
+    train_sizes, train_scores, test_scores_bnb = learning_curve(bnb, x, y, cv=cv, n_jobs=n_jobs,
                                                             train_sizes=train_sizes)
-    train_sizes1, train_scores1, test_scores1 = learning_curve(estimator2, x, y, cv=cv, n_jobs=n_jobs,
+    train_sizes1, train_scores1, test_scores_prc = learning_curve(prc, x, y, cv=cv, n_jobs=n_jobs,
                                                                train_sizes=train_sizes)
 
-    # Calcolo la media degli attributi dello score
-    train_scores_mean = np.mean(train_scores, axis=1)
-    train_scores_std = np.std(train_scores, axis=1)
-    test_scores_mean = np.mean(test_scores, axis=1)
-    test_scores_std = np.std(test_scores, axis=1)
+    test_scores_mean_bnb = np.mean(test_scores_bnb, axis=1)
 
-    train_scores_mean1 = np.mean(train_scores1, axis=1)
-    train_scores_std1 = np.std(train_scores1, axis=1)
-    test_scores_mean1 = np.mean(test_scores1, axis=1)
-    test_scores_std1 = np.std(test_scores1, axis=1)
+    test_scores_mean_prc = np.mean(test_scores_prc, axis=1)
+
     plt.grid()
-    plt.fill_between(train_sizes, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, color="w")
-    plt.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, color="w")
 
-    plt.fill_between(train_sizes1, train_scores_mean1 - train_scores_std1, train_scores_mean1 + train_scores_std1,
-                     color="w")
-    plt.fill_between(train_sizes1, test_scores_mean1 - test_scores_std1, test_scores_mean1 + test_scores_std1,
-                     color="w")
-    plt.plot(train_sizes, test_scores_mean, 'o-', color="g", label="BernoulliNB")
-    plt.plot(train_sizes1, test_scores_mean1, 'o-', color="r", label="Perceptron")
+    plt.plot(train_sizes, test_scores_mean_bnb, 'o-', color="g", label="BernoulliNB")
+    plt.plot(train_sizes1, test_scores_mean_prc, 'o-', color="r", label="Perceptron")
     plt.legend(loc="best")
     return plt
 
@@ -66,162 +52,36 @@ class LemmaTokenizer(object):
 if __name__ == '__main__':
     remove = ('headers', 'footers', 'quotes')
     print("Reperimento Datasets: 20News groups", end=" ")
-    # train_20news = fetch_20newsgroups(subset='train', shuffle=True, remove=remove)  # , categories=categories)
-    # test_20news = fetch_20newsgroups(subset='test', shuffle=True, remove=remove)  # , categories=categories)
     X_20news, y_20news = fetch_20newsgroups(return_X_y=True, shuffle=True, remove=remove)
     print("Completato")
 
     print("Reperimento Datasets: Reuters", end=" ")
-    #Reuters.warmup()
-    #X_train_reuters = load_files(container_path='data-set/training-set', shuffle=True, random_state=42)
-    #X_test_reuters = load_files(container_path='data-set/test-set', shuffle=True, random_state=42)
-    X_reut, y_reut = Reuters.return_X_y_reut()
+    X_reut, y_reut_noTok = Reuters.return_X_y_reut()
     print("Completato")
 
     print("Vettorizzazione dei documenti")
-    vectorizer = CountVectorizer(tokenizer=LemmaTokenizer(), stop_words="english", min_df=3)  # , binary=True)
-    #X_vectorized_20news = vectorizer.fit_transform(X_20news)
-    # TR_vectorized_20News = vectorizer.fit_transform(train_20news.data)
-    # TE_vectorized_20News = vectorizer.transform(test_20news.data)
+    vectorizerMini = CountVectorizer(tokenizer=LemmaTokenizer())
+    y_reut = vectorizerMini.fit_transform(y_reut_noTok).indices
+
+    vectorizer = CountVectorizer(tokenizer=LemmaTokenizer(), stop_words="english", min_df=3)
+    X_vectorized_20news = vectorizer.fit_transform(X_20news)
     print("20News Completato")
     X_vectorized_reut = vectorizer.fit_transform(X_reut)
-    # TR_vectorized_reuters = vectorizer.fit_transform(X_train_reuters.data)
-    # TE_vectorized_reuters = vectorizer.transform(X_test_reuters.data)
     print("Reuters Completato")
 
+    print("Generazione grafici")
     bnb20 = BernoulliNB()
     prc20 = Perceptron()
-    print("Addestramento (fitting) delle reti per 20News groups:")
-    print("Bernoullian Naive Bayes", end=" ")
-    # bnb20.fit(TR_vectorized_20News, train_20news.target)
-    print("Completato")
-    print("Perceptron", end=" ")
-    # prc20.fit(TR_vectorized_20News, train_20news.target)
-    print("Completato")
+
+    Title1 = "Learning Curves on 20newsgroups"
+    plot_learning_curves(bnb20, prc20, Title1, X_vectorized_20news, y_20news)
+    plt.savefig("bnb_prc_20group.png", dpi=100)
+    plt.show()
 
     bnbre = BernoulliNB()
     prcre = Perceptron()
-    print("Addestramento (fitting) delle reti per Reuters:")
-    print("Bernoullian Naive Bayes", end=" ")
-    # bnbre.fit(TR_vectorized_reuters, X_train_reuters.target)
-    print("Completato")
-    print("Perceptron", end=" ")
-    # prcre.fit(TR_vectorized_reuters, X_train_reuters.target)
-    print("Completato")
 
-    print("Calcolo delle predizioni per 20News group")
-    # predict_bnb20 = bnb20.predict(TE_vectorized_20News)
-    # predict_prc20 = prc20.predict(TE_vectorized_20News)
-    print("Predizioni fatte per 20News group")
-    print("Calcolo delle predizioni per Reuters")
-    # predict_bnbreut = bnbre.predict(TE_vectorized_reuters)
-    # predict_prcreut = prcre.predict(TE_vectorized_reuters)
-    print("Predizioni fatte per Reuters")
-
-    print("Report Bernoullian Naive Bayes")
-    print("20News groups: ")
-    # report(test_20news.target, predict_bnb20, train_20news.target_names)
-    print("Reuters: ")
-    # report(X_test_reuters.target, predict_bnbreut, X_train_reuters.target_names)
-
-    # report
-
-    print("Report Perceptron")
-    print("20News groups: ")
-    # report(test_20news.target, predict_prc20, train_20news.target_names)
-    print("Reuters: ")
-    # report(X_test_reuters.target, predict_prcreut, X_test_reuters.target_names)
-
-    # report
-
-    print("Generazione grafici")
-    cv = ShuffleSplit(n_splits=100, test_size=0.2)  # , random_state=0)
-    train_sizes = np.logspace(np.log10(.1), np.log10(1.0), 8)
-    '''
-    Title1 = "Learning Curves on 20newsgroups"
     Title2 = "Learning Curves on Reuters-21578"
-
-    plot_learning_curves(bnb, prc, Title1, TR_vectorized_20News, train_20news.target,
-                                    n_jobs=1,
-                                    train_sizes=([0.01, 0.1, 0.2, 0.3, 0.4, 0.6, 0.7, 0.8, 0.9, 1.]))
-    plt.show()
-    plt.savefig('grafico1.png', bbox_inches='tight')
-
-    plot_learning_curves(bnb, prc, Title2, TR_vectorized_reut,
-                                    X_reuters.target, n_jobs=1,
-                                    train_sizes=([0.01, 0.1, 0.2, 0.3, 0.4, 0.6, 0.7, 0.8, 0.9, 1.]))
-    plt.show()
-    plt.savefig('grafico2.png', bbox_inches='tight')
-    '''
-    '''
-    train_sizes, train_scores, test_scores = learning_curve(bnb20, TR_vectorized_20News, train_20news.target, cv=cv,
-                                                            scoring="f1_weighted", train_sizes=train_sizes, n_jobs=-1)
-    plot_curve(train_sizes, train_scores, test_scores, title="Bernoulli Naive Bayes 20Group")
-    plt.show()
-
-    train_sizes = np.logspace(np.log10(.1), np.log10(1.0), 8)
-    train_sizes, train_scores, test_scores = learning_curve(prc20, TR_vectorized_20News, train_20news.target, cv=cv,
-                                                            scoring="f1_weighted", train_sizes=train_sizes, n_jobs=-1)
-    plot_curve(train_sizes, train_scores, test_scores, title="Perceptron 20Group")
-    plt.show()
-
-##############################################################################################
-    train_sizes = np.logspace(np.log10(.1), np.log10(1.0), 8)
-    train_sizes, train_scores, test_scores = learning_curve(bnbre, TR_vectorized_reuters, X_train_reuters.target, cv=cv,
-                                                            scoring="f1_weighted", train_sizes=train_sizes, n_jobs=-1)
-    plot_curve(train_sizes, train_scores, test_scores, title="Bernoulli Naive Bayes reuters")
-    plt.show()
-
-    train_sizes = np.logspace(np.log10(.1), np.log10(1.0), 8)
-    train_sizes, train_scores, test_scores = learning_curve(prcre, TR_vectorized_reuters, X_train_reuters.target, cv=cv,
-                                                            scoring="f1_weighted", train_sizes=train_sizes, n_jobs=-1)
-    plot_curve(train_sizes, train_scores, test_scores, title="Perceptron reuters")
-    plt.show()
-    plt.savefig('grafici.png')  # , bbox_inches='tight')
-
-    print("Fine")
-    '''
-    Title1 = "Learning Curves on 20newsgroups"
-    Title2 = "Learning Curves on Reuters-21578"
-
-    # plot_learning_curves(bnb20, prc20, Title1, TR_vectorized_20News, train_20news.target)
-    #plot_learning_curves(bnb20, prc20, Title1, X_vectorized_20news, y_20news)
-    #plt.savefig("bnb_prc_20group.png", dpi=100)
-    #plt.show()
-    #plt.savefig("primo.jpg")
-    # plot_learning_curves(bnbre, prcre, Title2, TR_vectorized_reuters, X_train_reuters.target)
-    #X_vectorized_reut = TR_vectorized_reuters
-    #y_reut = X_train_reuters.target
     plot_learning_curves(bnbre, prcre, Title2, X_vectorized_reut, y_reut)
-    plt.savefig("bnb_prc_Reuters.png",dpi = 100)
+    plt.savefig("bnb_prc_Reuters.png", dpi=100)
     plt.show()
-
-    '''
-    fig, axes = plt.subplots(3, 2, figsize=(10, 15))
-
-    title = "Learning Curves (Naive Bayes)"
-    # Cross validation with 100 iterations to get smoother mean test and train
-    # score curves, each time with 20% data randomly selected as a validation set.
-    cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
-
-    estimator = bnb
-    X = TR_vectorized_20News.append(TE_vectorized_20News)
-    y = train_20news.target.append(test_20news.target)
-    plot_learning_curve(estimator, title, X, y, axes=axes[:, 0], ylim=(0.7, 1.01),
-                        cv=cv, n_jobs=-1)
-
-    title = r"Learning Curves (Perceptron)"
-    # SVC is more expensive so we do a lower number of CV iterations:
-    cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
-    estimator = prc
-    plot_learning_curve(estimator, title, X, y, axes=axes[:, 1], ylim=(0.7, 1.01),
-                        cv=cv, n_jobs=-1)
-
-    plt.show()
-    '''
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-# Bernoullian naive bayes ->
-# https://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.BernoulliNB.html#sklearn.naive_bayes.BernoulliNB
-
-# Perceptron
-# https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.Perceptron.html#sklearn.linear_model.Perceptron
