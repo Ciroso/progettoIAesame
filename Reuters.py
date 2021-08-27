14b263e878ef949881d15b920dfd112bc2e8a225
Ciroso <arman.lagevardi@hotmail.com>
2021-08-27 17:16:11 +0200

    Task binario per reuters

--- Reuters.py
+++ Reuters.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 from bs4 import BeautifulSoup
 import os
+import pickle
 
 
 def top10categories(reuters_files, use_cache=True):
     if use_cache:
-        print("Utilizzo la cache di top10categories Reuters", end=" ")
+        # print("Utilizzo la cache di top10categories Reuters", end=" ")
         if Path(r"cacheTop10.txt").exists():
             with open("cacheTop10.txt", 'r') as f:
                 top10 = [line.rstrip('\n') for line in f]
@@ -59,15 +60,74 @@ def top10categories(reuters_files, use_cache=True):
     return Top10
 
 
-def return_X_y_reut():
-    cwf = os.getcwd()
-    reut_files = []
-    for i in range(0, 22):
-        if i < 10:
-            reut_files.append(cwf + "/reuters21578/reut2-00" + str(i) + ".sgm")
-        else:
-            reut_files.append(cwf + "/reuters21578/reut2-0" + str(i) + ".sgm")
+def file_list():
+    if Path(r"cache_list.txt").exists():
+        with open("cache_list.txt", 'r') as f:
+            cache_list = [line.rstrip('\n') for line in f]
+        return cache_list
+    else:
+        cwf = os.getcwd()
+        reut_files = []
+        for i in range(0, 22):
+            if i < 10:
+                reut_files.append(cwf + "/reuters21578/reut2-00" + str(i) + ".sgm")
+            else:
+                reut_files.append(cwf + "/reuters21578/reut2-0" + str(i) + ".sgm")
+        with open("cache_list.txt", 'w') as f:
+            for s in reut_files:
+                f.write(str(s).replace("['", "").replace("']", "") + '\n')
+        return reut_files
 
+
+def return_X_y_reut(selected_category):
+    if Path(r"cache_reuters/cache_binary_" + selected_category + "_X").exists() and Path(
+            r"cache_reuters/cache_binary_" + selected_category + "_y").exists():
+        with open("cache_reuters/cache_binary_" + selected_category + "_X", "rb") as f:
+            X = pickle.load(f)
+        f.close()
+        with open("cache_reuters/cache_binary_" + selected_category + "_y", "rb") as f:
+            y = pickle.load(f)
+        return X, y
+    else:
+        reut_files = file_list()
+        top10cat = top10categories(reut_files)
+        X = []
+        y = []
+        for x in reut_files:
+            io = open(x, "r")
+            strr = io.read()
+            docs = strr.split("</REUTERS>")
+            for segment in docs:
+                if ('LEWISSPLIT="TRAIN"' or 'LEWISSPLIT="TEST"') in segment and 'TOPICS="YES"' in segment:
+                    inlist = False
+                    if "<D>" + str(selected_category).rstrip("']").lstrip("['") + "</D>" in segment:
+                        soup = BeautifulSoup(segment, features="html.parser")
+                        reut_body = soup.findAll("body")
+                        if len(reut_body) != 0:
+                            body = str(reut_body[0].string)
+                            X.append(body)
+                            y.append("cate")
+                    else:
+                        for cate in top10cat:
+                            if "<D>" + str(cate).rstrip("']").lstrip("['") + "</D>" in segment and not inlist:
+                                soup = BeautifulSoup(segment, features="html.parser")
+                                reut_body = soup.findAll("body")
+                                if len(reut_body) != 0:
+                                    inlist = True
+                                    body = str(reut_body[0].string)
+                                    X.append(body)
+                                    y.append("not_cate")
+        with open("cache_reuters/cache_binary_" + selected_category + "_X", 'wb') as f:
+            pickle.dump(X, f)
+        f.close()
+        with open("cache_reuters/cache_binary_" + selected_category + "_y", "wb") as f:
+            pickle.dump(y, f)
+        return X, y
+
+
+def return_X_y_reut_bnb():
+    # all_cat = dict()
+    reut_files = file_list()
     top10cat = top10categories(reut_files)
     X = []
     y = []
@@ -76,17 +136,22 @@ def return_X_y_reut():
         strr = io.read()
         docs = strr.split("</REUTERS>")
         for segment in docs:
-            if any("<D>" + str(cat).rstrip("']").lstrip("['") + "</D>" in segment for cat in top10cat):
-                if ('LEWISSPLIT="TRAIN"' or 'LEWISSPLIT="TEST"') in segment and 'TOPICS="YES"' in segment:
-                    inlist = False
-                    for cate in top10cat:
-                        if ("<D>" + str(cate).rstrip("']").lstrip("['") + "</D>" in segment) and inlist == False:
-                            soup = BeautifulSoup(segment, features="html.parser")
-                            reut_body = soup.findAll("body")
-                            if len(reut_body) != 0:
-                                body = str(reut_body[0].string)
-                                X.append(body)
-                                y.append(cate)
-                                inlist = True
+            # if any("<D>" + str(cat).rstrip("']").lstrip("['") + "</D>" in segment for cat in top10cat):
+            if ('LEWISSPLIT="TRAIN"' or 'LEWISSPLIT="TEST"') in segment and 'TOPICS="YES"' in segment:
+                inlist = False
+                # for cate in top10cat:
 
+                for cate in top10cat:
+                    if "<D>" + str(cate).rstrip("']").lstrip("['") + "</D>" in segment and not inlist:
+                        soup = BeautifulSoup(segment, features="html.parser")
+                        reut_body = soup.findAll("body")
+                        if len(reut_body) != 0:
+                            inlist = True
+                            body = str(reut_body[0].string)
+                            X.append(body)
+                            y.append(cate)
+
+                            # if cate not in all_cat:
+                            #    all_cat[cate] = list()
+                            # all_cat[cate].append(body)
     return X, y
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+import time
+
 from nltk import word_tokenize
 from nltk.stem.porter import PorterStemmer
 import re
@@ -10,9 +13,10 @@ from sklearn.datasets import fetch_20newsgroups
 import numpy as np
 import matplotlib.pyplot as plt
 import Reuters
+from sklearn.model_selection import train_test_split
 
 
-def plot_learning_curves(bnb, prc, title, x, y):
+def plot_learning_curves(bernoullian_nb, perceptron, title, x, y):
     n_jobs = -1
     train_sizes = np.logspace(np.log10(.1), np.log10(1.0), 8)
     cv = ShuffleSplit(n_splits=100, test_size=0.2)
@@ -22,10 +26,10 @@ def plot_learning_curves(bnb, prc, title, x, y):
     plt.xlabel("Numero di Esempi")
 
     plt.ylabel("Score")
-    train_sizes, train_scores, test_scores_bnb = learning_curve(bnb, x, y, cv=cv, n_jobs=n_jobs,
-                                                            train_sizes=train_sizes)
-    train_sizes1, train_scores1, test_scores_prc = learning_curve(prc, x, y, cv=cv, n_jobs=n_jobs,
-                                                               train_sizes=train_sizes)
+    train_sizes, train_scores, test_scores_bnb = learning_curve(bernoullian_nb, x, y, cv=cv, n_jobs=n_jobs,
+                                                                train_sizes=train_sizes)
+    train_sizes1, train_scores1, test_scores_prc = learning_curve(perceptron, x, y, cv=cv, n_jobs=n_jobs,
+                                                                  train_sizes=train_sizes)
 
     test_scores_mean_bnb = np.mean(test_scores_bnb, axis=1)
 
@@ -39,6 +43,47 @@ def plot_learning_curves(bnb, prc, title, x, y):
     return plt
 
 
+def plot_learning_curves_bnb(bernoullian_nb, title, x, y):
+    n_jobs = -1
+    train_sizes = np.logspace(np.log10(.1), np.log10(1.0), 8)
+    cv = ShuffleSplit(n_splits=100, test_size=0.2)
+
+    plt.figure()
+    plt.title(title)
+    plt.xlabel("Numero di Esempi")
+
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores_bnb = learning_curve(bernoullian_nb, x, y, cv=cv, n_jobs=n_jobs,
+                                                                train_sizes=train_sizes)
+
+    test_scores_mean_bnb = np.mean(test_scores_bnb, axis=1)
+
+    plt.grid()
+
+    plt.plot(train_sizes, test_scores_mean_bnb, 'o-', color="g", label="BernoulliNB")
+    plt.legend(loc="best")
+    return plt
+
+
+def learning_curves_for_means(classifier, x, y):
+    n_jobs = -1
+    train_sizes = np.logspace(np.log10(.1), np.log10(1.0), 8)
+    cv = ShuffleSplit(n_splits=100, test_size=0.2)
+
+    # plt.figure()
+    # plt.title(title)
+    # plt.xlabel("Numero di Esempi")
+
+    # plt.ylabel("Score")
+    # train_sizes, train_scores, test_scores_bnb = learning_curve(bernoullian_nb, x, y, cv=cv, n_jobs=n_jobs,
+    # train_sizes=train_sizes)
+    train_sizes1, train_scores1, test_scores_prc = learning_curve(classifier, x, y, cv=cv, n_jobs=n_jobs,
+                                                                  train_sizes=train_sizes)
+
+    # return test_scores_prc, train_sizes, train_sizes1
+    return test_scores_prc, train_sizes1
+
+
 class LemmaTokenizer(object):
     def __init__(self):
         self.regex = re.compile('[%s]' % re.escape(string.punctuation))
@@ -50,24 +95,22 @@ class LemmaTokenizer(object):
 
 
 if __name__ == '__main__':
+    start = time.time()
     remove = ('headers', 'footers', 'quotes')
-    print("Reperimento Datasets: 20News groups", end=" ")
-    X_20news, y_20news = fetch_20newsgroups(return_X_y=True, shuffle=True, remove=remove)
-    print("Completato")
 
-    print("Reperimento Datasets: Reuters", end=" ")
-    X_reut, y_reut_noTok = Reuters.return_X_y_reut()
-    print("Completato")
-
-    print("Vettorizzazione dei documenti")
+    #  FIXME
     vectorizerMini = CountVectorizer(tokenizer=LemmaTokenizer())
-    y_reut = vectorizerMini.fit_transform(y_reut_noTok).indices
+    vectorizer = CountVectorizer(tokenizer=LemmaTokenizer(), stop_words="english", min_df=3)
+    '''
+    print("Reperimento Datasets: 20News groups", end=" ")
+    X_20news, y_20news = fetch_20newsgroups(return_X_y=True, remove=remove)
+    print("Completato")
 
     vectorizer = CountVectorizer(tokenizer=LemmaTokenizer(), stop_words="english", min_df=3)
+
+    print("Vettorizzazione dei documenti")
     X_vectorized_20news = vectorizer.fit_transform(X_20news)
     print("20News Completato")
-    X_vectorized_reut = vectorizer.fit_transform(X_reut)
-    print("Reuters Completato")
 
     print("Generazione grafici")
     bnb20 = BernoulliNB()
@@ -77,12 +120,135 @@ if __name__ == '__main__':
     plot_learning_curves(bnb20, prc20, Title1, X_vectorized_20news, y_20news)
     plt.savefig("bnb_prc_20group.png", dpi=100)
     plt.show()
-
-    bnbre = BernoulliNB()
-    prcre = Perceptron()
+    '''
+    # ------------ FINE 20NEWS GROUP ------------
+    # -------------- INIZIO REUTERS --------------
 
     Title2 = "Learning Curves on Reuters-21578"
-    plot_learning_curves(bnbre, prcre, Title2, X_vectorized_reut, y_reut)
+
+    print("Reperimento Datasets: Reuters", end=" ")
+
+    top10cat = Reuters.top10categories(Reuters.file_list())
+    scores_bnb = 0
+    scores_prc = 0
+    train_sizes = 0
+    train_sizes1 = 0
+    count = 0
+    first_run = True
+    historic_score_bnb = []
+    historic_score_prc = []
+    for cat in top10cat:
+        count += 1
+        print("In esecuzione: ", cat, " (", count, ")/ 10")
+        X_reut, y_reut_noTok = Reuters.return_X_y_reut(cat)
+
+        # X_reut_shuffled, y_reut_noTok_shuffled = shuffle_in_unison(X_reut, y_reut_noTok)
+
+        c = list(zip(X_reut, y_reut_noTok))
+
+        random.shuffle(c)
+
+        X_reut_shuffled, y_reut_noTok_shuffled = zip(*c)
+
+        X_vectorized_reut = vectorizer.fit_transform(X_reut_shuffled)
+        y_reut = vectorizerMini.fit_transform(y_reut_noTok_shuffled).indices
+
+        # -----------------------------------------------------------------------------------------------------
+        # shuffled_X, shuffled_y = shuffle_in_unison(X_vectorized_reut, y_reut)
+        # CUT by scikit
+        # X_train, X_test, y_train, y_test = train_test_split(shuffled_X, shuffled_y, test_size=0.2)
+        X_train, X_test, y_train, y_test = train_test_split(X_vectorized_reut, y_reut, test_size=0.2)
+        clf1 = BernoulliNB()
+        clf2 = Perceptron()
+
+        clf1.fit(X_train, y_train)
+        clf2.fit(X_train, y_train)
+        y_predict1 = clf1.predict(X_test)
+        y_predict2 = clf2.predict(X_test)
+        score1 = clf1.score(X_test, y_test)
+        score2 = clf2.score(X_test, y_test)
+        print("Score BernoulliNB: ", score1)
+        print("Score2 Perceptron: ", score2)
+        historic_score_bnb.append(score1)
+        historic_score_prc.append(score2)
+        from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+        # print(classification_report(y_test, y_predict1))
+        # print("-----------------------------------------------------------------------------------------------------")
+        # print(classification_report(y_test, y_predict2))
+
+        # -----------------------------------------------------------------------------------------------------
+
+        score_bnb, train_sizes = learning_curves_for_means(BernoulliNB(), X_vectorized_reut, y_reut)  # alpha=0.01
+        score_prc, train_sizes1 = learning_curves_for_means(Perceptron(), X_vectorized_reut, y_reut)  # max_iter=50
+        if first_run:
+            scores_bnb = score_bnb
+            scores_prc = score_prc
+            first_run = False
+        else:
+            scores_bnb = (np.array(scores_bnb) + np.array(score_bnb)) / 2
+            scores_prc = (np.array(scores_prc) + np.array(score_prc)) / 2
+
+            #  ------------------------------------------------------------------------
+        '''
+        plt.figure()
+        plt.title(Title2 + " - " + cat)
+        plt.xlabel("Numero di Esempi")
+
+        plt.ylabel("Score")
+        mean_bnb = np.mean(scores_bnb, axis=1)
+        mean_prc = np.mean(scores_prc, axis=1)
+
+        mean_bnbs = np.mean(score_bnb, axis=1)
+        mean_prcs = np.mean(score_prc, axis=1)
+
+        plt.grid()
+
+        plt.plot(train_sizes, mean_bnb, 'o-', color="g", label="BernoulliNB")
+        plt.plot(train_sizes1, mean_prc, 'o-', color="r", label="Perceptron")
+
+        plt.plot(train_sizes, mean_bnbs, 'o-', color="b", label="BernoulliNB - Single")
+        plt.plot(train_sizes1, mean_prcs, 'o-', color="y", label="Perceptron - Single")
+        plt.legend(loc="best")
+        # return plt
+
+        plt.savefig("bnb_prc_Reuters.png", dpi=100)
+        plt.show()
+    '''
+        # scores_bnb.append(score_bnb)
+        # scores_prc.append(scores_prc)
+    '''
+    X_reut, y_reut_noTok = Reuters.return_X_y_reut_bnb()
+    vectorizerMini = CountVectorizer(tokenizer=LemmaTokenizer())
+    y_reut = vectorizerMini.fit_transform(y_reut_noTok).indices
+    X_vectorized_reut = vectorizer.fit_transform(X_reut)
+    scores_bnb, train_sizes = learning_curves_for_means(BernoulliNB(), X_vectorized_reut, y_reut)
+    '''
+    mean_bnb = np.mean(scores_bnb, axis=1)
+    mean_prc = np.mean(scores_prc, axis=1)
+
+    plt.figure()
+    plt.title(Title2)
+    plt.xlabel("Numero di Esempi")
+    plt.ylabel("Score")
+    plt.grid()
+    plt.plot(train_sizes, mean_bnb, 'o-', color="g", label="BernoulliNB")
+    plt.plot(train_sizes1, mean_prc, 'o-', color="r", label="Perceptron")
+    plt.legend(loc="best")
+    # return plt
+
     plt.savefig("bnb_prc_Reuters.png", dpi=100)
     plt.show()
+    # ----------------------------------------
 
+    plt.figure()
+    plt.ylabel("Score")
+    plt.xlabel("Categorie")
+    plt.plot(top10cat, historic_score_bnb, 'o-', color="g", label="BernoulliNB")
+    plt.plot(top10cat, historic_score_prc, 'o-', color="r", label="Perceptron")
+    plt.legend(loc="best")
+    plt.grid()
+    plt.savefig("bnb_prc_Reuters_score.png", dpi=100)
+    plt.show()
+
+    print("Elapsed time: ", time.time() - start)
